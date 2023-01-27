d393ca7c3c677562b743da1cfef7dbe89f70cf4c
OnurIlgaz <117534459+OnurIlgaz@users.noreply.github.com>
2023-01-27 16:59:15 +0300

    Clickable

--- app/src/main/java/com/example/sudoku/sudoku_view.kt
+++ app/src/main/java/com/example/sudoku/sudoku_view.kt
@@ -1,29 +1,41 @@
 package com.example.sudoku
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 
+@Suppress("UNREACHABLE_CODE")
 class SudokuView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
     private var size = 9
     private var sqrtsize=3
 
     private var cellSizePixels = 0F
 
+    private var selectedRow = -1
+    private var selectedCol = -1
+
     private val thickLinePaint = Paint().apply{
         style = Paint.Style.STROKE
         color = Color.BLACK
         strokeWidth = 4F
     }
-
     private val thinLinePaint = Paint().apply{
         style = Paint.Style.STROKE
         color = Color.BLACK
         strokeWidth = 2F
+    }
+    private val selectPaint = Paint().apply{
+        style= Paint.Style.FILL_AND_STROKE
+        color = Color.parseColor("#6ead3a")
+    }
+    private val conflictPaint = Paint().apply{
+        style= Paint.Style.FILL_AND_STROKE
+        color = Color.parseColor("#efedef")
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -31,13 +43,32 @@ class SudokuView(context: Context, attributeSet: AttributeSet) : View(context, a
         val sizePixels: Int = Math.min(widthMeasureSpec, heightMeasureSpec)
         setMeasuredDimension(sizePixels, sizePixels)
     }
-
+    private fun fillCell(canvas: Canvas, r:Int, c:Int, paint:Paint){
+        canvas.drawRect(r*cellSizePixels, c*cellSizePixels, (r+1)*cellSizePixels, (c+1)*cellSizePixels, paint)
+    }
+    private fun fillCells(canvas : Canvas){
+        if(selectedRow == -1 || selectedCol==-1) return
+        for(r in 0..size){
+            for(c in 0..size){
+                if(r == selectedRow && c == selectedCol){
+                    fillCell(canvas, r, c, selectPaint)
+                }
+                else if(r == selectedRow || c == selectedCol){
+                    fillCell(canvas, r, c, conflictPaint)
+                }
+                else if(r/sqrtsize == selectedRow/sqrtsize && c/sqrtsize == selectedCol/sqrtsize){
+                    fillCell(canvas, r, c, conflictPaint)
+                }
+            }
+        }
+    }
     override fun onDraw(canvas: Canvas){
         cellSizePixels=(width/size).toFloat()
-        canvas.drawRect(0F, 0F, width.toFloat(), height.toFloat(), thickLinePaint)
+        fillCells(canvas)
         drawLines(canvas)
     }
     private fun drawLines(canvas:Canvas){
+        canvas.drawRect(0F, 0F, width.toFloat(), height.toFloat(), thickLinePaint)
         for(i in 1..size){
 
             val paintToUse = when(i % sqrtsize){
@@ -60,4 +91,21 @@ class SudokuView(context: Context, attributeSet: AttributeSet) : View(context, a
             )
         }
     }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return when(event?.action){
+            MotionEvent.ACTION_DOWN -> {
+                handleTouch(event.x, event.y)
+                return true
+            }
+            else -> return false
+        }
+    }
+    private fun handleTouch(x: Float, y: Float) {
+        selectedRow = (x/ cellSizePixels).toInt()
+        selectedCol = (y/ cellSizePixels).toInt()
+        invalidate()
+    }
+
 }
